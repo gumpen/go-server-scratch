630c96136d8c4e6b42bfadf5350e87e2f7df2dd4
gumpen <shuhei1993727@gmail.com>
2021-02-05 04:29:28 +0900

    add primitive logging middleware

--- main.go
+++ main.go
@@ -3,12 +3,27 @@ package main
 import (
 	"fmt"
 	"html"
+	"log"
 	"net/http"
+	"time"
 )
 
+func loggingHandler(next http.HandlerFunc) http.HandlerFunc {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		t1 := time.Now()
+		next.ServeHTTP(w, r)
+		t2 := time.Now()
+		log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
-	})
+	http.HandleFunc("/", loggingHandler(handle))
 	http.ListenAndServe("127.0.0.1:8080", nil)
 }
